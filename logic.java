1bdaff80a872cc6beb4258d091e28ef691757037
HelloWorld-jh <79819812+HelloWorld-jh@users.noreply.github.com>
2021-04-09 16:59:50 +0900

    Add files via upload

---
+++ logic.java
@@ -0,0 +1,33 @@
+package week3_day5;
+
+public class logic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Car;
+		String Pole = null;
+		
+		String Pass = null;
+		String R;
+		String Sense = null;
+		String Call;
+		
+		if (Sense==Pole) {
+			System.out.println("차량이 장애물을 감지합니다");
+		
+			}
+		
+		else if (Pole == Pass) {
+		 System.out.println("후진합니다");
+		 System.out.println("주차선에 안착합니다");
+		}else if ( Pole != Pass) {
+			System.out.println("장애물과 충돌합니다");
+			System.out.println("보험사를 호출합니다");
+		}
+		
+		
+		
+		
+	}
+
+}
